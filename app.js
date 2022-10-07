349bc9296cd4f396d3e57a9ca7e9de4dc4fc0244
Shahbaz Ashiq <shahbazashiq88@gmail.com>
2022-10-07 00:21:01 -0700

    Ready to commit

---
+++ app.js
@@ -0,0 +1,74 @@
+function button (){
+    var a = document.getElementById("hvr1")
+    var name = a.value;
+
+var b = document.getElementById("hvr2");
+var fathername = b.value;
+
+var c = document.getElementById("hvr3");
+var dob = c.value;
+
+var d = document.getElementById("hvr4");
+var nationality = d.value;
+
+var e = document.getElementById("hvr5");
+var gender = e.value;
+
+var f = document.getElementById("hvr6");
+var religion = f.value;
+var g = document.getElementById("hvr7");
+var address = f.value;
+var h = document.getElementById("hvr8");
+var cnic = f.value;
+var i = document.getElementById("hvr9");
+var cont = f.value;
+var j = document.getElementById("hvr10");
+var mark = f.value;
+var k = document.getElementById("hvr11");
+var postcode = f.value;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var first1 = document.getElementById("hrr1")
+first1.innerHTML=name;
+var first2 = document.getElementById("hrr2")
+first2.innerHTML=fathername;
+var first3 = document.getElementById("hrr3")
+first3.innerHTML=dob;
+var first4 = document.getElementById("hrr4")
+first4.innerHTML=nationality;
+var first5 = document.getElementById("hrr5")
+first5.innerHTML=gender;
+var first6 = document.getElementById("hrr6")
+first6.innerHTML=gender;
+var first7 = document.getElementById("hrr7")
+first7.innerHTML=gender;
+var first8 = document.getElementById("hrr8")
+first8.innerHTML=gender;
+var first9 = document.getElementById("hrr9")
+first9.innerHTML=gender;
+var first10 = document.getElementById("hrr10")
+first10.innerHTML=gender;
+var first11 = document.getElementById("hrr11")
+first11.innerHTML=gender;
+}
